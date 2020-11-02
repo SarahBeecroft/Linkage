236d6f4de03837f6d4aaad0b38f3927227c5342e
SarahBeecroft <sarah.beecroft@uwa.edu.au>
2020-11-02 14:31:47 +0800

    Update auto_complete_linkage_merlin.sh

--- auto_complete_linkage_merlin.sh
+++ auto_complete_linkage_merlin.sh
@@ -1,24 +1,28 @@
-#! /bin/bash
+#!/bin/bash
 
-file=$1
-model=$2
+echo "Enter the base name for your input files (i.e. family prefix, i.e OPDM): "  
+read input_prefix  
+echo "The base name is $input_prefix"  
+echo  
+echo "Enter inheritance pattern (AD for autosomal dominant, AR for autosomal recessive). X-linked is not available as a pre-made file: "  
+read model
+echo "Inheritance pattern is $model"
+
 #model options are dominant or recessive. haven't made an x-linked file yet
 
-cd /home/san/sbeecroft/old_linkdatagen/
-
-nice perl vcf2linkdatagen.pl -annotfile annotHapMap2.txt -pop CEU -mindepth 20 -missingness 0 -idlist $file.vcflist.txt > $file.brlmm
-nice perl linkdatagen.pl -data m -pedfile $file.ped -whichSamplesFile $file.ws -callFile $file.brlmm -annotFile annotHapMap2.txt -pop CEU -binsize 0.3 -mindist 0.15 -prog me -outputDir $file.HapMap2
-cd /home/san/sbeecroft/old_linkdatagen/$file.HapMap2_merlin/genome/
-cp /home/san/sbeecroft/merlin-1.1.2/executables/$model.param.tbl temp.tbl
-nice merlin -d merlin_autosome_$file.dat -p merlin_genome_$file.ped -f merlin_genome_$file.freq -m merlin_genome_$file.map --prefix merlin_autosome_$file --error
-nice pedwipe -d merlin_autosome_$file.dat -p merlin_genome_$file.ped -e merlin_autosome_$file.err
-mv wiped.dat merlin_autosome_wiped_$file.dat
-mv wiped.ped merlin_autosome_wiped_$file.ped
-nice minx -d merlin_X_$file.dat -p merlin_genome_$file.ped -f merlin_genome_$file.freq -m merlin_genome_$file.map --prefix merlin_X_$file --error
-nice pedwipe -d merlin_X_$file.dat -p merlin_genome_$file.ped -e merlin_X_$file.err
-mv wiped.dat merlin_X_wiped_$file.dat
-mv wiped.ped merlin_X_wiped_$file.ped
+perl vcf2linkdatagen.pl -annotfile annotHapMap2U.txt -pop CEU -mindepth 20 -missingness 0 -idlist $input_prefix.vcflist.txt > $input_prefix.brlmm
+perl linkdatagen.pl -data m -pedfile $input_prefix.ped -whichSamplesFile $input_prefix.ws -callFile $input_prefix.brlmm -annotFile annotHapMap2U.txt -pop CEU -binsize 0.3 -mindist 0.15 -prog me -outputDir $input_prefix.HapMap2
+cd $input_prefix.HapMap2_merlin/genome/
+cp $model.param.tbl temp.tbl
+merlin -d merlin_autosome_$input_prefix.dat -p merlin_genome_$input_prefix.ped -f merlin_genome_$input_prefix.freq -m merlin_genome_$input_prefix.map --prefix merlin_autosome_$input_prefix --error
+pedwipe -d merlin_autosome_$input_prefix.dat -p merlin_genome_$input_prefix.ped -e merlin_autosome_$input_prefix.err
+mv wiped.dat merlin_autosome_wiped_$input_prefix.dat
+mv wiped.ped merlin_autosome_wiped_$input_prefix.ped
+minx -d merlin_X_$input_prefix.dat -p merlin_genome_$input_prefix.ped -f merlin_genome_$input_prefix.freq -m merlin_genome_$input_prefix.map --prefix merlin_X_$input_prefix --error
+pedwipe -d merlin_X_$input_prefix.dat -p merlin_genome_$input_prefix.ped -e merlin_X_$input_prefix.err
+mv wiped.dat merlin_X_wiped_$input_prefix.dat
+mv wiped.ped merlin_X_wiped_$input_prefix.ped
 rm wiped.freq
 mv temp.tbl $model.param.tbl
-nice merlin -d merlin_autosome_wiped_$file.dat -p merlin_autosome_wiped_$file.ped -f merlin_genome_$file.freq -m merlin_genome_$file.map --smallswap --megabytes:9999 --founders --MarkerNames --pairs --ibd --extended --best --exp --model $model.param.tbl --pdf --tabulate --prefix merlin_autosome_$file
-nice minx -d merlin_X_wiped_$file.dat -p merlin_X_wiped_$file.ped -f merlin_genome_$file.freq -m merlin_genome_$file.map --smallswap --megabytes:9999 --founders --MarkerNames --pairs --ibd --extended --best --exp --model $model.param.tbl --pdf --tabulate --prefix merlin_X_$file
+merlin -d merlin_autosome_wiped_$input_prefix.dat -p merlin_autosome_wiped_$input_prefix.ped -f merlin_genome_$input_prefix.freq -m merlin_genome_$input_prefix.map --smallswap --megabytes:9999 --founders --MarkerNames --pairs --ibd --extended --best --exp --model $model.param.tbl --pdf --tabulate --prefix merlin_autosome_$input_prefix
+minx -d merlin_X_wiped_$input_prefix.dat -p merlin_X_wiped_$input_prefix.ped -f merlin_genome_$input_prefix.freq -m merlin_genome_$input_prefix.map --smallswap --megabytes:9999 --founders --MarkerNames --pairs --ibd --extended --best --exp --model $model.param.tbl --pdf --tabulate --prefix merlin_X_$input_prefix
