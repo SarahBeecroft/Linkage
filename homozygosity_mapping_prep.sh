dcdd056b33f7e8ed28b85c49d163742eb27eb4ef
SarahBeecroft <sarah.beecroft@uwa.edu.au>
2020-11-02 15:02:19 +0800

    Update homozygosity_mapping_prep.sh

--- homozygosity_mapping_prep.sh
+++ homozygosity_mapping_prep.sh
@@ -1,12 +1,9 @@
-#! /bin/bash
+#!/bin/bash
 
-for file in maori_homozygosity_mapping
+echo 'usage is ./homozygosity_mapping_prep.sh <file_prefix> (e.g. OPDM)'
 
-do
+file=$1
 
-cd /home/san/sbeecroft/old_linkdatagen/
+perl vcf2linkdatagen.pl -annotfile annotHapMap2U.txt -pop CEU -mindepth 20 -missingness 0 -idlist $file.vcflist.txt > $file.brlmm
 
-nice perl vcf2linkdatagen.pl -annotfile annotHapMap2.txt -pop CEU -mindepth 20 -missingness 0 -idlist $file.vcflist.txt > $file.brlmm \
-&& nice perl linkdatagen.pl -data m -pedfile $file.ped -whichSamplesFile $file.ws -callFile $file.brlmm -annotFile annotHapMap2.txt -pop CEU -binsize 0.3 -mindist 0.15 -prog cp -outputDir $file.HapMap2 \
-
-done
+perl linkdatagen.pl -data m -pedfile $file.ped -whichSamplesFile $file.ws -callFile $file.brlmm -annotFile annotHapMap2U.txt -pop CEU -binsize 0.3 -mindist 0.15 -prog cp -outputDir $file.HapMap2
